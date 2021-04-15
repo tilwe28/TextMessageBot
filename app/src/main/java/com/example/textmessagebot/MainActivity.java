4a259444576ba5e1e218353ef9d6714d86f412b3
Anurag <anurag.tilwe6@gmail.com>
2021-04-15 10:29:55 -0400

    Receving Messages

--- app/src/main/java/com/example/textmessagebot/MainActivity.java
+++ app/src/main/java/com/example/textmessagebot/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.textmessagebot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_receivedMessage;
+
+    MessageReceiver messageReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_receivedMessage = findViewById(R.id.id_textView_receivedMessage);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS}, 1);
+            return;
+        } else Toast.makeText(this, "SMS Permssions Granted", Toast.LENGTH_SHORT);
+
+        messageReceiver = new MessageReceiver();
+        IntentFilter messageIntentFilter = new IntentFilter();
+        messageIntentFilter.addAction(android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        registerReceiver(messageReceiver, messageIntentFilter);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("TAG", "MessageReceiver");
+            Bundle bundle = intent.getExtras();
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            SmsMessage[] messages = new SmsMessage[pdus.length];
+            for (int i=0; i<messages.length; i++) {
+                messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], bundle.getString("format"));
+                Log.d("TAG", "From " + messages[i].getOriginatingAddress() + " Message Body " + i + ": " + messages[i].getMessageBody());
+            }
+        }
     }
 }
