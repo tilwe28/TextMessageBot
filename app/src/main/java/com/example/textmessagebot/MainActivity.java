204911ffa5354d120c5a2da9e3fe90e30adb95f3
Anurag <anurag.tilwe6@gmail.com>
2021-04-22 14:59:45 -0400

    Send SMS working

--- app/src/main/java/com/example/textmessagebot/MainActivity.java
+++ app/src/main/java/com/example/textmessagebot/MainActivity.java
@@ -5,16 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_receivedMessage;
 
     MessageReceiver messageReceiver;
+    SmsManager smsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +32,29 @@ public class MainActivity extends AppCompatActivity {
 
         tv_receivedMessage = findViewById(R.id.id_textView_receivedMessage);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS}, 1);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS, Manifest.permission.READ_PHONE_STATE}, 0);
             return;
-        } else Toast.makeText(this, "SMS Permssions Granted", Toast.LENGTH_SHORT);
-
-        messageReceiver = new MessageReceiver();
-        IntentFilter messageIntentFilter = new IntentFilter();
-        messageIntentFilter.addAction(android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
-        registerReceiver(messageReceiver, messageIntentFilter);
+        } else {
+            Log.d("TAG", "Permissions already Granted");
+            messageReceiver = new MessageReceiver();
+            IntentFilter messageIntentFilter = new IntentFilter();
+            messageIntentFilter.addAction(android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+            registerReceiver(messageReceiver, messageIntentFilter);
+        }
 
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("TAG", "Perimissions Result");
+        if (permissions.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            messageReceiver = new MessageReceiver();
+            IntentFilter messageIntentFilter = new IntentFilter();
+            messageIntentFilter.addAction(android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+            registerReceiver(messageReceiver, messageIntentFilter);
+        }
     }
 
     public class MessageReceiver extends BroadcastReceiver {
@@ -56,10 +64,23 @@ public class MainActivity extends AppCompatActivity {
             Bundle bundle = intent.getExtras();
             Object[] pdus = (Object[]) bundle.get("pdus");
             SmsMessage[] messages = new SmsMessage[pdus.length];
+            String phoneNumber = "";
             for (int i=0; i<messages.length; i++) {
                 messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], bundle.getString("format"));
-                Log.d("TAG", "From " + messages[i].getOriginatingAddress() + " Message Body " + i + ": " + messages[i].getMessageBody());
+                phoneNumber = messages[i].getOriginatingAddress();
+                Log.d("TAG", "From " + phoneNumber + " Message Body " + i + ": " + messages[i].getMessageBody());
             }
+
+            try {
+                smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber, null, "Wassup", null, null);
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Message Failed", Toast.LENGTH_LONG).show();
+                Log.d("TAG", e.toString());
+                e.printStackTrace();
+            }
+
+            Log.d("TAG", "Replied");
         }
     }
 }
